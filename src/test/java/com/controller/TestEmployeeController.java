5a557a3a2f1290f9b3e108213fa54a8320b96d5a
Mohamed Mazhar <mazhar.1998@hotmail.com>
2026-03-06 17:46:12 +0100

    lts update

---
+++ src/test/java/com/controller/TestEmployeeController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestEmployeeController {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
